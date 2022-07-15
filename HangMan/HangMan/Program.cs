7fdfdbafa8b30bb14a650dd53dc3c9b94ee31b51
Karol Hartwig <43039961+xGuti@users.noreply.github.com>
2022-07-15 23:51:20 +0200

    Initial commit
    Created project, words.txt file (only polish version right now) and words.sql where will be created sql query.

---
+++ HangMan/HangMan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFile = @"C:\Users\Guti\Documents\GitHub\HangMan\words.txt"; //local path
+            string lines = File.ReadAllText(textFile);
+
+            lines = Regex.Replace(lines, ",", String.Empty);
+            lines = Regex.Replace(lines, "\\n", String.Empty);
+
+            //Console.WriteLine(lines);
+
+            string[] words = lines.Split(' ');//Split() Split(' ')  Actual number of words
+            Console.WriteLine(words.Count()); // 915      912               933
+                                              //WHY?
+        }
+    }
+}
